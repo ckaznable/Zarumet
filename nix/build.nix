2a951a25244c93f17cf1941bbf7c4c6cf618d72b
Immelancholy <lenalowes0@gmail.com>
2025-12-11 04:04:21 +0000

    fix(nix): configure FFmpeg and clang for Rust build
    - Sets environment variables for FFmpeg and libclang paths.
    - Provides system CC/CXX and relevant CFLAGS/LDFLAGS.
    - Adds necessary build and native build inputs for FFmpeg and clang.

--- nix/build.nix
+++ nix/build.nix
@@ -1,6 +1,15 @@
 {
+  lib,
   rustToolchain,
   rustPlatform,
+  pkg-config,
+  ffmpeg,
+  llvmPackages,
+  clang,
+  glibc,
+  libclang,
+  stdenv,
+  ffmpeg_7,
 }: let
   zarumetCargoLock = builtins.fromTOML (builtins.readFile ../Cargo.toml);
 in
@@ -8,6 +17,37 @@ in
     pname = "zarumet";
     version = zarumetCargoLock.package.version;
     src = ../.;
+
+    LIBCLANG_PATH = "${libclang.lib}/lib";
+    PKG_CONFIG_PATH = "${ffmpeg_7.dev}/lib/pkgconfig";
+    FFMPEG_PKG_CONFIG_PATH = "${ffmpeg_7.dev}/lib/pkgconfig";
+
+    # Force use of system FFmpeg
+    FFMPEG_STATIC = "false";
+    FFMPEG_LIBS_DIR = "${ffmpeg_7.out}/lib";
+    FFMPEG_INCLUDE_DIR = "${ffmpeg_7.dev}/include";
+    VCPKG_ROOT = "";
+
+    # Set proper C compiler flags and override hardcoded paths
+    CC = "${stdenv.cc}/bin/cc";
+    CXX = "${stdenv.cc}/bin/c++";
+    CFLAGS = "-I${ffmpeg_7.dev}/include -I${glibc.dev}/include";
+    CXXFLAGS = "-I${ffmpeg_7.dev}/include -I${glibc.dev}/include";
+    LDFLAGS = "-L${ffmpeg_7.out}/lib";
+    BINDGEN_EXTRA_CLANG_ARGS = "-I${ffmpeg_7.dev}/include -I${glibc.dev}/include -I${stdenv.cc.cc}/include/c++/${stdenv.cc.cc.version} -I${stdenv.cc.cc}/include/c++/${stdenv.cc.cc.version}/${stdenv.hostPlatform.config}";
+
+    nativeBuildInputs = [
+      pkg-config
+      clang
+    ];
+
+    buildInputs = [
+      ffmpeg_7
+      ffmpeg_7.dev
+      libclang
+      glibc
+      glibc.dev
+    ];
 
     cargoLock.lockFile = ../Cargo.lock;
   }
